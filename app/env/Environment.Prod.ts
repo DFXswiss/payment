075e65db15aa2740c9b980ac5ae16ff72a2a9317
David May <david.leo.may@gmail.com>
2021-06-16 22:21:07 +0200

    Using prod environment

--- app/env/Environment.Prod.ts
+++ app/env/Environment.Prod.ts
@@ -1,7 +1,3 @@
-// TODO: use during build
-// # fix --release-channel not available for web yet
-// cp -f app/env/Environment.Prod.ts app/env/Environment.ts
-
 export const Environment = {
   api: {
     baseUrl: "https://api.fiat2defi.ch/api/v1"

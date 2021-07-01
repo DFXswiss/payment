abcf0c19390e6ca9d09d3e8528220cca11cc9036
David May <david.leo.may@gmail.com>
2021-07-01 10:58:42 +0200

    Using title element

--- app/elements/Texts.tsx
+++ app/elements/Texts.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { TextStyle, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Title, Text } from "react-native-paper";
 import Colors from "../config/Colors";
 import AppStyles from "../styles/AppStyles";
 
 export const H1 = ({ text, style }: { text: string; style?: TextStyle }) => (
-  <Text style={[AppStyles.h1, style]}>{text}</Text>
+  <Title style={[AppStyles.h1, style]}>{text}</Title>
 );
 export const H2 = ({ text, style }: { text: string; style?: TextStyle }) => (
-  <Text style={[AppStyles.h2, style]}>{text}</Text>
+  <Title style={[AppStyles.h2, style]}>{text}</Title>
 );
 export const H3 = ({ text, style }: { text: string; style?: TextStyle }) => (
-  <Text style={[AppStyles.h3, style]}>{text}</Text>
+  <Title style={[AppStyles.h3, style]}>{text}</Title>
 );
 
 export const Alert = ({ label }: { label: string }) => (

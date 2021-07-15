0271ee4ba2ed01fb2eda5d76763d33800dc0f457
David May <david.leo.may@gmail.com>
2021-07-15 22:21:16 +0200

    Refactoring

--- app/services/ApiService.ts
+++ app/services/ApiService.ts
@@ -22,23 +22,23 @@ const CountryUrl = "country";
 
 // --- AUTH --- //
 export const signIn = (credentials?: Credentials): Promise<string> => {
-  return fetchFrom<AuthResponse>(`${BaseUrl}/${AuthUrl}/signIn`, buildInit("POST", undefined, credentials))
+  return fetchFrom<AuthResponse>(`${BaseUrl}/${AuthUrl}/signIn`, "POST", credentials)
     .then((resp) => resp.accessToken);
 };
 
 export const signUp = (user: NewUser): Promise<string> => {
-  return fetchFrom<AuthResponse>(`${BaseUrl}/${AuthUrl}/signUp`, buildInit("POST", undefined, toNewUserDto(user)))
+  return fetchFrom<AuthResponse>(`${BaseUrl}/${AuthUrl}/signUp`, "POST", toNewUserDto(user))
     .then((resp) => resp.accessToken);
 }
 
 // --- USER --- //
 export const getUser = (): Promise<User> => {
-  return AuthService.Session.then((s) => fetchFrom<UserDto>(`${BaseUrl}/${UserUrl}`, buildInit("GET", s)))
+  return fetchFrom<UserDto>(`${BaseUrl}/${UserUrl}`)
     .then((dto: UserDto) => fromUserDto(dto));
 };
 
 export const putUser = (user: User): Promise<User> => {
-  return AuthService.Session.then((session) => fetchFrom<UserDto>(`${BaseUrl}/${UserUrl}/${session.address}`, buildInit("PUT", session, toUserDto(user))))
+  return fetchFrom<UserDto>(`${BaseUrl}/${UserUrl}/${user.address}`, "PUT", toUserDto(user))
     .then((dto: UserDto) => fromUserDto(dto));
 };
 
@@ -54,32 +54,26 @@ export const getActiveRoutes = (): Promise<PaymentRoutes> => {
 };
 
 const getRoutesDto = (): Promise<PaymentRoutesDto> => {
-  return AuthService.Session.then((session) =>
-    fetchFrom<PaymentRoutesDto>(`${BaseUrl}/${RouteUrl}`, buildInit("GET", session))
-  );
+  return fetchFrom<PaymentRoutesDto>(`${BaseUrl}/${RouteUrl}`);
 };
 
 export const postBuyRoute = (route: BuyRoute): Promise<BuyRoute> => {
-  return AuthService.Session
-    .then((session) => fetchFrom<BuyRouteDto>(`${BaseUrl}/${BuyUrl}`, buildInit("POST", session, toBuyRouteDto(route))))
+  return fetchFrom<BuyRouteDto>(`${BaseUrl}/${BuyUrl}`, "POST", toBuyRouteDto(route))
     .then((dto) => fromBuyRouteDto(dto));
 };
 
 export const putBuyRoute = (route: BuyRoute): Promise<BuyRoute> => {
-  return AuthService.Session
-    .then((session) => fetchFrom<BuyRouteDto>(`${BaseUrl}/${BuyUrl}/${route.id}`, buildInit("PUT", session, toBuyRouteDto(route))))
+  return fetchFrom<BuyRouteDto>(`${BaseUrl}/${BuyUrl}/${route.id}`, "PUT", toBuyRouteDto(route))
     .then((dto) => fromBuyRouteDto(dto));
 };
 
 export const postSellRoute = (route: SellRoute): Promise<SellRoute> => {
-  return AuthService.Session
-    .then((session) => fetchFrom<SellRouteDto>(`${BaseUrl}/${SellUrl}`, buildInit("POST", session, toSellRouteDto(route))) )
+  return fetchFrom<SellRouteDto>(`${BaseUrl}/${SellUrl}`, "POST", toSellRouteDto(route))
     .then((dto) => fromSellRouteDto(dto));
 };
 
 export const putSellRoute = (route: SellRoute): Promise<SellRoute> => {
-  return AuthService.Session
-    .then((session) => fetchFrom<SellRouteDto>(`${BaseUrl}/${SellUrl}/${route.id}`, buildInit("PUT", session, toSellRouteDto(route))))
+  return fetchFrom<SellRouteDto>(`${BaseUrl}/${SellUrl}/${route.id}`, "PUT", toSellRouteDto(route))
     .then((dto) => fromSellRouteDto(dto));
 };
 
@@ -97,23 +91,25 @@ export const getCountries = (): Promise<Country[]> => {
 };
 
 // --- HELPERS --- //
-const buildInit = (method: "GET" | "PUT" | "POST", session?: Session, data?: any): RequestInit => ({
+const fetchFrom = <T>(url: string, method: "GET" | "PUT" | "POST" = "GET", data?: any): Promise<T> => {
+  return AuthService.Session
+      .then((session) => buildInit(method, session, data))
+      .then((init) => fetch(url, init))
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((body) => {
+          throw body;
+        });
+      });
+};
+
+const buildInit = (method: "GET" | "PUT" | "POST", session: Session, data?: any): RequestInit => ({
   method: method,
   headers: {
     "Content-Type": "application/json",
-    Authorization: session ? "Bearer " + session.accessToken : "",
+    Authorization: session.accessToken ? "Bearer " + session.accessToken : "",
   },
   body: JSON.stringify(data),
 });
-
-const fetchFrom = <T>(url: string, init?: RequestInit): Promise<T> => {
-  return fetch(url, init).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    return response.json().then((body) => {
-      throw body;
-    });
-  });
-};
-};

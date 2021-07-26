62ad948c34565c896a0affda5833fec2916b4ac4
David May <david.leo.may@gmail.com>
2021-07-27 00:01:51 +0200

    Added logout on 401

--- app/services/ApiService.ts
+++ app/services/ApiService.ts
@@ -1,5 +1,5 @@
 import { Environment } from "../env/Environment";
-import { AuthResponse } from "../models/ApiDto";
+import { ApiError, AuthResponse } from "../models/ApiDto";
 import { Asset } from "../models/Asset";
 import { BuyRoute, BuyRouteDto, fromBuyRouteDto, toBuyRouteDto } from "../models/BuyRoute";
 import { Country } from "../models/Country";
@@ -7,6 +7,7 @@ import { Fiat } from "../models/Fiat";
 import { fromSellRouteDto, SellRoute, SellRouteDto, toSellRouteDto } from "../models/SellRoute";
 import { fromUserDto, NewUser, toNewUserDto, toUserDto, User, UserDto } from "../models/User";
 import AuthService, { Credentials, Session } from "./AuthService";
+import SessionService from "./SessionService";
 
 const BaseUrl = Environment.api.baseUrl;
 const AuthUrl = "auth";
@@ -16,7 +17,6 @@ const SellUrl = "sell";
 const AssetUrl = "asset";
 const FiatUrl = "fiat";
 const CountryUrl = "country";
-
 
 // --- AUTH --- //
 export const signIn = (credentials?: Credentials): Promise<string> => {
@@ -87,17 +87,24 @@ export const getCountries = (): Promise<Country[]> => {
 // --- HELPERS --- //
 const fetchFrom = <T>(url: string, method: "GET" | "PUT" | "POST" = "GET", data?: any): Promise<T> => {
   return AuthService.Session
-      // TODO: logout if 401 return!
-      .then((session) => buildInit(method, session, data))
-      .then((init) => fetch(url, init))
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return response.json().then((body) => {
-          throw body;
-        });
+    .then((session) => buildInit(method, session, data))
+    .then((init) => fetch(url, init))
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response.json().then((body) => {
+        throw body;
       });
+    })
+    // TODO: this throws state update error (on HomeScreen)
+    .catch((error: ApiError) => {
+      if (error.statusCode === 401) {
+        AuthService.deleteSession();
+      }
+
+      throw error;
+    });
 };
 
 const buildInit = (method: "GET" | "PUT" | "POST", session: Session, data?: any): RequestInit => ({

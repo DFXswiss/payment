615cf77ba080c7969ac1fc00cb2810430fe9c851
David May <david.leo.may@gmail.com>
2021-07-30 21:13:43 +0200

    Fixed memory leak

--- app/components/util/VideoPlayer.tsx
+++ app/components/util/VideoPlayer.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScaledSize, Dimensions } from "react-native";
 import Sizes from "../../config/Sizes";
 
 // use react-native-webview on native?
@@ -10,9 +8,11 @@ const VideoPlayer = ({ src, maxWidth }: { src: string; maxWidth: number }) => {
     Math.min(maxWidth ?? Infinity, screenWidth - 2 * Sizes.AppPadding);
 
   const [videoWidth, setVideoWidth] = useState(getVideoWidth(Dimensions.get("window").width));
+  const dimensionListener = ({ window }: { window: ScaledSize }) => setVideoWidth(getVideoWidth(window.width));
 
   useEffect(() => {
-    Dimensions.addEventListener("change", ({ window }) => setVideoWidth(getVideoWidth(window.width)));
+    Dimensions.addEventListener("change", dimensionListener);
+    return () => Dimensions.removeEventListener("change", dimensionListener);
   }, []);
 
   return React.createElement("iframe", {

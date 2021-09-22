93e35879f6f6e4c205ee6815c56ce2e67c402fdf
David May <david.leo.may@gmail.com>
2021-09-22 16:40:25 +0200

    Added missing files

---
+++ app/screens/CfpScreen.tsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { StyleSheet, View } from "react-native";
+import { StackedBarChart } from "react-native-chart-kit";
+import { AbstractChartConfig } from "react-native-chart-kit/dist/AbstractChart";
+import { StackedBarChartData } from "react-native-chart-kit/dist/StackedBarChart";
+import { ActivityIndicator, DataTable, Paragraph, RadioButton, TouchableRipple } from "react-native-paper";
+import AppLayout from "../components/AppLayout";
+import Colors from "../config/Colors";
+import { SpacerV } from "../elements/Spacers";
+import { CompactRow, CompactCell } from "../elements/Tables";
+import { H1, H3 } from "../elements/Texts";
+import withSession from "../hocs/withSession";
+import useAuthGuard from "../hooks/useAuthGuard";
+import { CfpResult } from "../models/CfpResult";
+import { getAllCfpResults, getDfxCfpResults } from "../services/ApiService";
+import { Session } from "../services/AuthService";
+import NotificationService from "../services/NotificationService";
+import AppStyles from "../styles/AppStyles";
+
+const CfpScreen = ({ session }: { session?: Session }) => {
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [cfpFilter, setCfpFilter] = React.useState("dfx");
+  const [cfpResults, setCfpResults] = useState<CfpResult[]>();
+
+  useAuthGuard(session);
+
+  useEffect(() => {
+    setIsLoading(true);
+    (cfpFilter === "dfx" ? getDfxCfpResults() : getAllCfpResults())
+      .then(setCfpResults)
+      .catch(() => NotificationService.show(t("feedback.load_failed")))
+      .finally(() => setIsLoading(false));
+  }, [cfpFilter]);
+
+  const getData = (result: CfpResult): StackedBarChartData => {
+    return {
+      labels: [""],
+      legend: ["Yes", "Neutral", "No"],
+      data: [[result.yes, result.neutral, result.no]],
+      barColors: [Colors.Success, Colors.LightGrey, Colors.Error],
+    };
+  };
+  const config: AbstractChartConfig = {
+    backgroundColor: Colors.White,
+    backgroundGradientFrom: Colors.White,
+    backgroundGradientTo: Colors.White,
+    decimalPlaces: 0,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    propsForBackgroundLines: {
+      stroke: Colors.White,
+    },
+    propsForHorizontalLabels: {
+      stroke: Colors.White,
+      fill: Colors.White,
+    },
+  };
+
+  return (
+    <AppLayout>
+      <View style={[AppStyles.container, AppStyles.alignCenter]}>
+        <H1 text={t("cfp.title")} />
+        <SpacerV height={30} />
+
+        {isLoading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <>
+            <View style={AppStyles.containerHorizontalWrap}>
+              <TouchableRipple onPress={() => setCfpFilter("dfx")}>
+                <View style={styles.radioRow}>
+                  <View pointerEvents="none">
+                    <RadioButton value="dfx" status={cfpFilter === "dfx" ? "checked" : "unchecked"} />
+                  </View>
+                  <Paragraph>DFX CFPs only</Paragraph>
+                </View>
+              </TouchableRipple>
+              <TouchableRipple onPress={() => setCfpFilter("all")}>
+                <View style={styles.radioRow}>
+                  <View pointerEvents="none">
+                    <RadioButton value="all" status={cfpFilter === "all" ? "checked" : "unchecked"} />
+                  </View>
+                  <Paragraph>All CFPs</Paragraph>
+                </View>
+              </TouchableRipple>
+            </View>
+            <SpacerV height={30} />
+            {cfpResults
+              ?.sort((a, b) => a.number - b.number)
+              .map((result) => (
+                <View key={result.number} style={{ width: "100%" }}>
+                  <H3 text={result.title} style={{ textAlign: "center" }} />
+                  <View style={styles.cfpContainer}>
+                    <View>
+                      <DataTable style={{ width: 250 }}>
+                        <CompactRow>
+                          <CompactCell>Number</CompactCell>
+                          <CompactCell>#{result.number}</CompactCell>
+                        </CompactRow>
+                        <CompactRow>
+                          <CompactCell>Voting</CompactCell>
+                          <CompactCell>
+                            {result.yes} / {result.neutral} / {result.no}
+                          </CompactCell>
+                        </CompactRow>
+                        <CompactRow>
+                          <CompactCell>#Votes</CompactCell>
+                          <CompactCell>{result.votes}</CompactCell>
+                        </CompactRow>
+                        <CompactRow>
+                          <CompactCell>Vote turnout</CompactCell>
+                          <CompactCell>{result.voteTurnout}</CompactCell>
+                        </CompactRow>
+                        <CompactRow>
+                          <CompactCell>Current result</CompactCell>
+                          <CompactCell>{result.currentResult}</CompactCell>
+                        </CompactRow>
+                      </DataTable>
+                    </View>
+                    <View>
+                      <StackedBarChart
+                        data={getData(result)}
+                        width={250}
+                        height={200}
+                        chartConfig={config}
+                        hideLegend={false}
+                        style={AppStyles.mt20}
+                      />
+                    </View>
+                  </View>
+                  <SpacerV height={40} />
+                </View>
+              ))}
+          </>
+        )}
+      </View>
+    </AppLayout>
+  );
+};
+
+const styles = StyleSheet.create({
+  cfpContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap-reverse",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  radioRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+});
+
+export default withSession(CfpScreen);

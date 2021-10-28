d0b05c31363fb49779f0807f7b859406f2fc0ede
Stanislaw Findeisen <sfindeisen@users.noreply.github.com>
2021-10-28 14:46:17 +0200

    use test db

--- app/env/Environment.ts
+++ app/env/Environment.ts
@@ -2,7 +2,7 @@ export const Environment = {
   debug: true,
   defaultLanguage: "DE",
   api: {
-    baseUrl: "http://localhost:3000/v1",
-    refUrl: "http://localhost:3000/app?code=",
+    baseUrl: "https://app-dfx-api-dev.azurewebsites.net/v1",
+    refUrl: "https://app-dfx-api-dev.azurewebsites.net/app?code=",
   },
 };

22b829d3e577d42ba4d3219175430b4576a6f3ea
David May <david.leo.may@gmail.com>
2021-12-09 22:37:11 +0100

    Adapted header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,24 +9,47 @@ import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { IconButton } from "react-native-paper";
 import { useDevice } from "../hooks/useDevice";
 import HeaderContent from "./HeaderContent";
+import { tailwind } from "@tailwind";
+
+const Logo = ({ session }: { session?: Session }) => {
+  const nav = useNavigation<DrawerNavigationProp<any>>();
+  const goHome = () => nav.navigate(session?.isLoggedIn ? Routes.Home : Routes.Login);
+
+  return (
+    <TouchableOpacity activeOpacity={1} style={styles.logoTouch} onPress={goHome}>
+      <Image style={styles.logo} source={require("../assets/logo.jpg")} />
+    </TouchableOpacity>
+  );
+};
 
 const Header = ({ session }: { session?: Session }) => {
   const device = useDevice();
   const nav = useNavigation<DrawerNavigationProp<any>>();
 
-  const goHome = () => nav.navigate(session?.isLoggedIn ? Routes.Home : Routes.Login);
+  const backNavigationRoute = "";
 
   return (
     <View style={[AppStyles.containerHorizontal, styles.container]}>
-      {!device.SM && <IconButton icon="menu" onPress={() => nav.toggleDrawer()} style={AppStyles.mra} />}
-
-      <TouchableOpacity activeOpacity={1} style={styles.logoTouch} onPress={goHome}>
-        <Image style={styles.logo} source={require("../assets/logo.jpg")} />
-      </TouchableOpacity>
-
-      <View style={[AppStyles.container, !device.SM && AppStyles.noDisplay]}>
-        <HeaderContent />
-      </View>
+      {device.SM ? (
+        <>
+          <Logo session={session} />
+          <View style={[AppStyles.container, !device.SM && AppStyles.noDisplay]}>
+            <HeaderContent />
+          </View>
+        </>
+      ) : (
+        <View style={tailwind('flex-1 flex-row justify-between items-center')}>
+          <View style={!backNavigationRoute && AppStyles.hidden}>
+            <IconButton icon="chevron-left" onPress={() => nav.navigate(backNavigationRoute)} style={AppStyles.mra} />
+          </View>
+          <View>
+            <Logo session={session} />
+          </View>
+          <View>
+            <IconButton icon="menu" onPress={() => nav.toggleDrawer()} style={AppStyles.mra} />
+          </View>
+        </View>
+      )}
     </View>
   );
 };

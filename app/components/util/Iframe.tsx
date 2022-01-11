87d3e18e8f94ca63143e1fcc4adb7aba19565682
David May <david.leo.may@gmail.com>
2022-01-11 01:39:27 +0100

    Removed memo

--- app/components/util/Iframe.tsx
+++ app/components/util/Iframe.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 // use react-native-webview on native?
-const Iframe = React.memo(({ src }: { src: string }) => {
+const Iframe = ({ src }: { src: string }) => {
   return React.createElement("iframe", {
     src: src,
     frameBorder: "0",
     style: { flex: 1 },
   });
-});
+};
 
 export default Iframe;

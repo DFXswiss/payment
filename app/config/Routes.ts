c9340d94f53f4d79a9d50445c29d6ad160ba5bc2
David May <david.leo.may@gmail.com>
2022-01-25 23:57:40 +0100

    Improved page titles

--- app/config/Routes.ts
+++ app/config/Routes.ts
@@ -1,11 +1,11 @@
 const PageTitle = "DFX";
 
 export default {
-  Home: `${PageTitle}: Home`,
-  Login: `${PageTitle}: Login`,
-  Gtc: `${PageTitle}: Legal`,
-  Cfp: `${PageTitle}: CFPs`,
-  Admin: `${PageTitle}: Admin`,
-  ChatBot: `${PageTitle}: ChatBot`,
-  NotFound: `${PageTitle}: NotFound`,
+  Home: `${PageTitle} - Home`,
+  Login: `${PageTitle} - Login`,
+  Gtc: `${PageTitle} - Legal`,
+  Cfp: `${PageTitle} - CFPs`,
+  Admin: `${PageTitle} - Admin`,
+  ChatBot: `${PageTitle} - ChatBot`,
+  NotFound: `${PageTitle} - NotFound`,
 };

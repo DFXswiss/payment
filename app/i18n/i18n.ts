2262c70b0266d3e43ac97acde3179f75b4688846
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2022-01-26 00:22:43 +0100

    Add ES resource

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -4,11 +4,12 @@ import { Environment } from "../env/Environment";
 
 i18n.use(initReactI18next).init({
   resources: {
-      DE: require("./de.json"),
-      EN: require("./en.json"),
-      FR: require("./fr.json"),
-      IT: require("./it.json"),
-      PT: require("./pt.json"),
+    DE: require("./de.json"),
+    EN: require("./en.json"),
+    FR: require("./fr.json"),
+    IT: require("./it.json"),
+    PT: require("./pt.json"),
+    ES: require("./es.json"),
   },
   fallbackLng: Environment.defaultLanguage,
   debug: Environment.debug,

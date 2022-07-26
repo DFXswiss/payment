ca8b9b57678b1ac0d03975bed1d4cc2bdb51c7c2
ThaBrad <18183189+ThaBrad@users.noreply.github.com>
2022-07-26 11:02:41 +0200

    [DEV-413] cleanup (so custom DFX voting can be used for future use)

--- app/screens/SpecialVotingScreen.tsx
+++ app/screens/SpecialVotingScreen.tsx
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { StyleSheet, View } from "react-native";
-import { StackedBarChart } from "react-native-chart-kit";
-import { AbstractChartConfig } from "react-native-chart-kit/dist/AbstractChart";
-import { StackedBarChartData } from "react-native-chart-kit/dist/StackedBarChart";
-import { DataTable, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import AppLayout from "../components/AppLayout";
 import Loading from "../components/util/Loading";
-import Colors from "../config/Colors";
-import { DeFiButton } from "../elements/Buttons";
 import { RadioButton } from "../elements/RadioButton";
 import { SpacerV } from "../elements/Spacers";
-import { CompactRow, CompactCell } from "../elements/Tables";
 import { H1, H3, T5 } from "../elements/Texts";
 import withSession from "../hocs/withSession";
 import useAuthGuard from "../hooks/useAuthGuard";
@@ -22,15 +16,11 @@ import { Session } from "../services/AuthService";
 import NotificationService from "../services/NotificationService";
 import AppStyles from "../styles/AppStyles";
 
-const DfxCfpNumbers: number[] = [];
-
 const SpecialVotingScreen = ({ session }: { session?: Session }) => {
   const { t } = useTranslation();
   const [isLoading, setIsLoading] = useState(true);
-  const [cfpFilter, setCfpFilter] = useState<"all" | "dfx">("all");
   const [cfpResults, setCfpResults] = useState<CfpResult[]>();
   const [canVote, setCanVote] = useState(false);
-  const [isVotingOpen, setIsVotingOpen] = useState(false);
   const [votes, setVotes] = useState<CfpVotes | undefined>();
   const [isSaving, setIsSaving] = useState<{ number: Number; vote: CfpVote } | undefined>();
 
@@ -85,24 +75,10 @@ const SpecialVotingScreen = ({ session }: { session?: Session }) => {
         setCfpResults(results);
         setVotes(votes);
         setCanVote(user.stakingBalance >= 100);
-        setIsVotingOpen(settings.cfpVotingOpen);
       })
       .catch(() => NotificationService.error(t("feedback.load_failed")))
       .finally(() => setIsLoading(false));
   }, []);
-
-  // const getData = (result: CfpResult): StackedBarChartData => {
-  //   return {
-  //     labels: [],
-  //     legend: [
-  //       `${t("cfp.yes")} (${Math.round((result.totalVotes.yes / result.totalVotes.total) * 100)}%)`,
-  //       `${t("cfp.neutral")} (${Math.round((result.totalVotes.neutral / result.totalVotes.total) * 100)}%)`,
-  //       `${t("cfp.no")} (${Math.round((result.totalVotes.no / result.totalVotes.total) * 100)}%)`,
-  //     ],
-  //     data: [[], [result.totalVotes.yes, result.totalVotes.neutral, result.totalVotes.no]],
-  //     barColors: [Colors.Success, Colors.LightGrey, Colors.Error],
-  //   };
-  // };
 
   const onVote = (number: number, vote: CfpVote) => {
     setVotes((votes) => {
@@ -115,18 +91,6 @@ const SpecialVotingScreen = ({ session }: { session?: Session }) => {
     });
   };
 
-  const config: AbstractChartConfig = {
-    backgroundColor: Colors.Blue,
-    backgroundGradientFrom: Colors.Blue,
-    backgroundGradientTo: Colors.Blue,
-    decimalPlaces: 0,
-    color: (opacity = 1) => `rgba(255,  255,  255,  ${opacity})`,
-    labelColor: (opacity = 1) => `rgba(255,  255,  255,  ${opacity})`,
-    propsForBackgroundLines: {
-      stroke: Colors.Blue,
-    },
-  };
-
   return (
     <AppLayout>
       <View style={[AppStyles.container, AppStyles.alignCenter]}>
@@ -137,20 +101,6 @@ const SpecialVotingScreen = ({ session }: { session?: Session }) => {
           <Loading size="large" />
         ) : (
           <>
-            {DfxCfpNumbers.length > 0 && (
-              <>
-                <View style={AppStyles.containerHorizontalWrap}>
-                  <RadioButton
-                    label={t("cfp.dfx_only")}
-                    onPress={() => setCfpFilter("dfx")}
-                    checked={cfpFilter === "dfx"}
-                  />
-                  <RadioButton label={t("cfp.all")} onPress={() => setCfpFilter("all")} checked={cfpFilter === "all"} />
-                </View>
-                <SpacerV height={50} />
-              </>
-            )}
-
             {cfpResults === undefined || (cfpResults.length === 0) && (
               <>
                 <H3 text={t("specialVoting.ended")} />
@@ -159,46 +109,41 @@ const SpecialVotingScreen = ({ session }: { session?: Session }) => {
             )}
 
             {cfpResults
-              ?.filter((r) => cfpFilter === "all" || DfxCfpNumbers.includes(r.number))
               ?.sort((a, b) => a.number - b.number)
               .map((result) => (
                 <View key={result.number} style={{ width: "100%" }}>
                   <View style={AppStyles.halfWidth} >
-                    <T5 text={t("specialVoting.explanation") + "\n\n\n"} /* style={AppStyles.center}  */ />
+                    <T5 text={t("specialVoting.explanation") + "\n\n\n"} />
                   </View>
 
-                  {(canVote || isSpecialVoting(result)) && (
-                    <>
-                      <SpacerV />
-                      <Text style={[AppStyles.center, AppStyles.b]}>{t("cfp.your_vote")}</Text>
-                      <View style={[AppStyles.containerHorizontalWrap, styles.voteContainer]}>
-                        <RadioButton
-                          label={t("specialVoting.collectively")}
-                          onPress={() => onVote(result.number, CfpVote.YES)}
-                          checked={votes?.[result.number] === CfpVote.YES}
-                          disabled={!canVote}
-                          loading={isSaving?.number === result.number && isSaving.vote === CfpVote.YES}
-                        />
-                        <RadioButton
-                          label={t("specialVoting.proportionally")}
-                          onPress={() => onVote(result.number, CfpVote.NO)}
-                          checked={votes?.[result.number] === CfpVote.NO}
-                          disabled={!canVote}
-                          loading={isSaving?.number === result.number && isSaving.vote === CfpVote.NO}
-                        />
-                        <RadioButton
-                          label={t("cfp.neutral")}
-                          onPress={() => onVote(result.number, CfpVote.NEUTRAL)}
-                          checked={(votes?.[result.number] ?? CfpVote.NEUTRAL) === CfpVote.NEUTRAL}
-                          disabled={!canVote}
-                          loading={isSaving?.number === result.number && isSaving.vote === CfpVote.NEUTRAL}
-                        />
-                      </View>
-                    </>
-                  )}
+                  <SpacerV />
+                  <Text style={[AppStyles.center, AppStyles.b]}>{t("cfp.your_vote")}</Text>
+                  <View style={[AppStyles.containerHorizontalWrap, styles.voteContainer]}>
+                    <RadioButton
+                      label={t("specialVoting.collectively")}
+                      onPress={() => onVote(result.number, CfpVote.YES)}
+                      checked={votes?.[result.number] === CfpVote.YES}
+                      disabled={!canVote}
+                      loading={isSaving?.number === result.number && isSaving.vote === CfpVote.YES}
+                    />
+                    <RadioButton
+                      label={t("specialVoting.proportionally")}
+                      onPress={() => onVote(result.number, CfpVote.NO)}
+                      checked={votes?.[result.number] === CfpVote.NO}
+                      disabled={!canVote}
+                      loading={isSaving?.number === result.number && isSaving.vote === CfpVote.NO}
+                    />
+                    <RadioButton
+                      label={t("cfp.neutral")}
+                      onPress={() => onVote(result.number, CfpVote.NEUTRAL)}
+                      checked={(votes?.[result.number] ?? CfpVote.NEUTRAL) === CfpVote.NEUTRAL}
+                      disabled={!canVote}
+                      loading={isSaving?.number === result.number && isSaving.vote === CfpVote.NEUTRAL}
+                    />
+                  </View>
 
                   <View style={AppStyles.halfWidth} >
-                    <T5 text={"\n\n\n" + t("specialVoting.description")} /* style={AppStyles.center}  */ />
+                    <T5 text={"\n\n\n" + t("specialVoting.description")} />
                   </View>
 
                   <SpacerV height={50} />
@@ -212,55 +157,9 @@ const SpecialVotingScreen = ({ session }: { session?: Session }) => {
 };
 
 const styles = StyleSheet.create({
-  cfpContainer: {
-    flexDirection: "row",
-    flexWrap: "wrap-reverse",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  chartContainer: {
-    width: 250,
-    height: 180,
-    alignItems: "flex-end",
-    overflow: "hidden",
-  },
-  radioRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingHorizontal: 16,
-  },
   voteContainer: {
     justifyContent: "center",
   },
 });
 
 export default withSession(SpecialVotingScreen);
-
-const description1 = `
-Jeder Nutzer des DFI Stakingservice von DFX erhält eine gewichtete Stimme auf Basis der investierten DFI. Zur Wahl stehen zwei alternative Abstimmungsmechanismen. 
-
-
-Auswahlmöglichkeiten:
-
-DFX soll mit allen Masternodes als Kollektiv mit ja/nein/neutral abstimmen
-DFX soll mit allen Masternodes anteilig der abgegebenen Stimmen mit ja/nein/neutral abstimmen
-Neutral
-`
-
-const description2 = `
-Kollektivbetrachtung:
-
-Abhängig der Wahlergebnisse der jeweiligen CFPs/DFIPs von der DFX Community stimmt DFX mit allen Masternodes für seine Kunden ab. 
-> 50% - DFX votet mit allen Masternodes „nein“
-50% - DFX votet mit allen Masternodes „neutral“
->50% - DFX votet mit allen Masternodes „ja“
-Die Kollektivbetrachtung wird nur angewendet, so lange DFX <10 % der DeFiChain Masternodes für seine Kunden betreibt. Wird dieser Schwellenwert überschritten wird DFX automatisch die anteilige Betrachtung wählen
-
-
-Anteilige Betrachtung:
-
-Abhängig der Wahlergebnisse der jeweiligen CFPs von der DFX Community stimmt DFX für seine Kunden anteilig der abgegeben Stimmen mit ja/nein/neutral ab
-
-
-`

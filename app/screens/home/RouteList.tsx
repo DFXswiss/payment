9bcb5e116cfba2c54f57419afbb95e43a64d4f0b
Krysh <krysh.developer@gmail.com>
2022-08-04 09:42:15 +0200

    [DEV-556] added crypto route refbonus after lost it on rebase

--- app/screens/home/RouteList.tsx
+++ app/screens/home/RouteList.tsx
@@ -243,7 +243,11 @@ const RouteList = ({
     },
     { condition: true, label: "model.route.blockchain", value: route.blockchain },
     { condition: true, label: "model.route.asset", value: route.asset?.name },
-    { condition: true, label: "model.route.fee", value: `${route.fee}%` },
+    {
+      condition: true,
+      label: "model.route.fee",
+      value: `${route.fee}%` + (route.refBonus ? ` (${route.refBonus}% ${t("model.route.ref_bonus")})` : ""),
+    },
     {
       condition: true,
       label: "model.route.volume",

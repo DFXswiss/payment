dbdded301a8a79062e15b11928ab03c50edd2e95
Krysh <krysh.developer@gmail.com>
2022-08-09 17:03:16 +0200

    [DEV-598] added btc login support

--- app/env/Environment.ts
+++ app/env/Environment.ts
@@ -1,7 +1,7 @@
 export const Environment = {
   debug: true,
   defaultLanguage: "DE",
-  addressFormat: /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40})$/,
+  addressFormat: /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39})$/,
   api: {
     baseUrl: "http://localhost:3000/v1",
     refUrl: "http://localhost:3000/app?code=",

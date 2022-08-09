ff37fe864a4441a2e453ee924830146372c07cb2
Krysh <krysh.developer@gmail.com>
2022-08-09 17:03:16 +0200

    [DEV-598] added btc login support

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -33,13 +33,13 @@ interface LoginData {
 
 const messageDefichain =
   "By signing this message, you confirm that you are the sole owner of the provided DeFiChain address and are in possession of its private key. Your ID:";
-const messageEthereum =
-  "By signing this message, you confirm that you are the sole owner of the provided Blockchain address and are in possession of its private key. Your ID:";
+const messageGeneral =
+  "By signing this message, you confirm that you are the sole owner of the provided Blockchain address. Your ID:";
 
 const signingCommand = (address: string) => {
-  const isEthereum = address.startsWith("0x");
-  const message = `${isEthereum ? messageEthereum : messageDefichain} ${address}`.split(" ").join("_");
-  return isEthereum ? message : `signmessage "${address}" "${message}"`;
+  const isDefichain = address.match(/^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41})$/) !== null
+  const message = `${isDefichain ? messageDefichain : messageGeneral} ${address}`.split(" ").join("_");
+  return isDefichain ? `signmessage "${address}" "${message}"` : message;
 };
 
 const LoginScreen = () => {

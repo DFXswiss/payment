828d46e0b6bdc2f38a6d262a1efbf5692c58c77c
Krysh <krysh.developer@gmail.com>
2022-08-10 10:29:15 +0200

    [DEV-598] added environment changes to dev too

--- app/env/Environment.dev.ts
+++ app/env/Environment.dev.ts
@@ -1,7 +1,7 @@
 export const Environment = {
   debug: true,
   defaultLanguage: "DE",
-  addressFormat: /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41})$/,
+  addressFormat: /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39})$/,
   api: {
     baseUrl: "https://dev.api.dfx.swiss/v1",
     refUrl: "https://dev.api.dfx.swiss/app?code=",

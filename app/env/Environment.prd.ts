8369a46886f2f7adc405598d6b673d1d67df2b73
Krysh <krysh.developer@gmail.com>
2022-08-18 14:42:05 +0200

    [DEV-635] moved signature regex to environments

--- app/env/Environment.prd.ts
+++ app/env/Environment.prd.ts
@@ -1,6 +1,7 @@
 export const Environment = {
   defaultLanguage: "DE",
   addressFormat: /^(8\w{33}|d\w{33}|d\w{41})$/,
+  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132})$/,
   api: {
     baseUrl: "https://api.dfx.swiss/v1",
     refUrl: "https://dfx.swiss/app?code=",

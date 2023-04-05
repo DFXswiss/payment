55559a594874c6c7ac23947f41addc5f14d25b12
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2023-04-05 16:30:08 +0200

    [DEV-1398] remove beta from crypto route, add all blockchains, enable DeFiChain to BTC (#224)
    * [DEV-1398] Remove beta from crypto route, add all blockchains to crypto route, enable DeFiChain to BTC
    
    * [DEV-1398] fix labels
    
    * [DEV-1398] Fixed labels
    
    ---------
    
    Co-authored-by: David May <david.leo.may@gmail.com>

--- app/models/Blockchain.ts
+++ app/models/Blockchain.ts
@@ -3,4 +3,10 @@ export enum Blockchain {
   BITCOIN = "Bitcoin",
   ETHEREUM = "Ethereum",
   BINANCE_SMART_CHAIN = "BinanceSmartChain",
+  OPTIMISM = "Optimism",
+  ARBITRUM = "Arbitrum",
+  POLYGON = "Polygon",
+  CARDANO = "Cardano",
 }
+
+export const AllowedCryptoBlockchains = [Blockchain.BITCOIN, Blockchain.DEFICHAIN];

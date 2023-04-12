0bbcab4166e2d63a2a7bc6ab737a170f15ec9d95
David May <david.leo.may@gmail.com>
2023-04-12 14:32:06 +0200

    [DEV-1431] Removed IBAN in buy route

--- app/models/BuyRoute.ts
+++ app/models/BuyRoute.ts
@@ -5,7 +5,6 @@ export interface BuyRouteDto {
   id: string;
   asset: Asset;
   bankUsage: string;
-  iban: string;
   volume: number;
   annualVolume: number;
   active: boolean;
@@ -17,7 +16,6 @@ export interface BuyRoute {
   id: string;
   asset: Asset;
   bankUsage: string;
-  iban: string;
   volume: number;
   annualVolume: number;
   active: boolean;
@@ -29,7 +27,6 @@ export const fromBuyRouteDto = (route: BuyRouteDto): BuyRoute => ({
   id: route.id,
   asset: route.asset,
   bankUsage: route.bankUsage,
-  iban: route.iban.replace(/(.{4})/g, "$1 "),
   active: route.active,
   volume: route.volume,
   annualVolume: route.annualVolume,
@@ -41,7 +38,6 @@ export const toBuyRouteDto = (route: BuyRoute): BuyRouteDto => ({
   id: route.id,
   asset: route.asset,
   bankUsage: route.bankUsage,
-  iban: route.iban.split(" ").join(""),
   active: route.active,
   volume: route.volume,
   annualVolume: route.annualVolume,

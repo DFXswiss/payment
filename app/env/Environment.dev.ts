1d57d5186df735c976b89ead35236919a8393a8c
Krysh <krysh.developer@gmail.com>
2023-04-27 11:01:27 +0200

    [DEV-1497] added address format and signature format for LN addresses

--- app/env/Environment.dev.ts
+++ app/env/Environment.dev.ts
@@ -1,8 +1,9 @@
 export const Environment = {
   debug: true,
   defaultLanguage: "DE",
-  addressFormat: /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39})$/,
-  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132})$/,
+  addressFormat:
+    /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|LN[A-Z0-9]{100}|ln[a-z0-9]{105})$/,
+  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104})$/,
   api: {
     baseUrl: "https://dev.api.dfx.swiss/v1",
     refUrl: "https://dev.api.dfx.swiss/app?code=",
--- app/env/Environment.prd.ts
+++ app/env/Environment.prd.ts
@@ -1,7 +1,8 @@
 export const Environment = {
   defaultLanguage: "DE",
-  addressFormat: /^(8\w{33}|d\w{33}|d\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39})$/,
-  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132})$/,
+  addressFormat:
+    /^(8\w{33}|d\w{33}|d\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|LN[A-Z0-9]{100}|ln[a-z0-9]{105})$/,
+  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104})$/,
   api: {
     baseUrl: "https://api.dfx.swiss/v1",
     refUrl: "https://dfx.swiss/app?code=",

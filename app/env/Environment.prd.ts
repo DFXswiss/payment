028c945b1470305b26d6ad2013a0393db77609ec
David May <85513542+davidleomay@users.noreply.github.com>
2023-06-07 18:43:05 +0200

    [DEV-1556] Alby & pub key login (#241)

--- app/env/Environment.prd.ts
+++ app/env/Environment.prd.ts
@@ -1,7 +1,8 @@
 export const Environment = {
   defaultLanguage: "DE",
-  addressFormat: /^(8\w{33}|d\w{33}|d\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|LNURL[A-Z0-9]{25,250})$/,
-  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104})$/,
+  addressFormat:
+    /^(8\w{33}|d\w{33}|d\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|LNURL[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66})$/,
+  signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104}|[a-z0-9]{142})$/,
   api: {
     baseUrl: "https://api.dfx.swiss/v1",
     refUrl: "https://dfx.swiss/app?code=",

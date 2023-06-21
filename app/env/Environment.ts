7059a9982341084ac5339e56c60190050e33def4
bernd2022 <bernd.nospam.2018@t-online.de>
2023-06-21 14:49:48 +0200

    [NO-TASK] added LNDHUB in address format

--- app/env/Environment.ts
+++ app/env/Environment.ts
@@ -2,7 +2,7 @@ export const Environment = {
   debug: true,
   defaultLanguage: "DE",
   addressFormat:
-    /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|LNURL[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66})$/,
+    /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|(LNURL|LNDHUB)[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66})$/,
   signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104}|[a-z0-9]{140,146})$/,
   api: {
     baseUrl: "http://localhost:3000/v1",

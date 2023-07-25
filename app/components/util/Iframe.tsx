1901def7875c23fedbff457e4e2c8c776056ac42
David May <david.leo.may@gmail.com>
2023-07-25 15:53:01 +0200

    [DEV-1650] Adapted Iframe settings

--- app/components/util/Iframe.tsx
+++ app/components/util/Iframe.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Sizes from "../../config/Sizes";
+
 // use react-native-webview on native?
 const Iframe = ({ src }: { src: string }) => {
   return React.createElement("iframe", {
     src: src,
     frameBorder: "0",
-    style: { flex: 1 },
+    style: { width: Sizes.AppWidth, height: "800px" },
     allow: "camera *; microphone *",
+    allowfullscreen: "",
   });
 };
 

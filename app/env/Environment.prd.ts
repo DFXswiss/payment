7aec1c7fd2b5cf230f5ec9d45cbf35ed239c5c03
bernd2022 <104787072+bernd2022@users.noreply.github.com>
2023-07-31 16:03:40 +0200

    [DEV-1531] multisig and taproot address format (#248)
    * [DEV-1531] multisig and taproot address format
    
    * [DEV-1531] changes after review

--- app/env/Environment.prd.ts
+++ app/env/Environment.prd.ts
@@ -1,7 +1,13 @@
+const bitcoinAddressFormat = "([13]|bc1)[a-zA-HJ-NP-Z0-9]{25,62}";
+const lightningAddressFormat = "(LNURL|LNDHUB)[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66}";
+const ethereumAddressFormat = "0x\\w{40}";
+const defichainAddressFormat = "8\\w{33}|d\\w{33}|d\\w{41}";
+
+const allAddressFormat = `${bitcoinAddressFormat}|${lightningAddressFormat}|${ethereumAddressFormat}|${defichainAddressFormat}`;
+
 export const Environment = {
   defaultLanguage: "DE",
-  addressFormat:
-    /^(8\w{33}|d\w{33}|d\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|(LNURL|LNDHUB)[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66})$/,
+  addressFormat: new RegExp(`^(${allAddressFormat})$`),
   signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104}|[a-z0-9]{140,146})$/,
   api: {
     baseUrl: "https://api.dfx.swiss/v1",

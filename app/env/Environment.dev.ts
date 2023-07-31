f36968af29c4f5985f6f2a6a1dcd2bc3d36a44c3
bernd2022 <bernd.nospam.2018@t-online.de>
2023-07-31 13:22:10 +0200

    [DEV-1531] multisig and taproot address format

--- app/env/Environment.dev.ts
+++ app/env/Environment.dev.ts
@@ -1,8 +1,32 @@
+function getBitcoinAddressFormat(): string {
+  return "([13]|bc1)[a-zA-HJ-NP-Z0-9]{25,62}";
+}
+
+function getLightningAddressFormat(): string {
+  return "(LNURL|LNDHUB)[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66}";
+}
+
+function getEthereumAddressFormat(): string {
+  return "0x\\w{40}";
+}
+
+function getDefichainAddressFormat(): string {
+  return "[78]\\w{33}|[td]\\w{33}|[td]\\w{41}";
+}
+
+function getAllAddressFormat(): string {
+  const bitcoin = getBitcoinAddressFormat();
+  const lightning = getLightningAddressFormat();
+  const ethereum = getEthereumAddressFormat();
+  const defichain = getDefichainAddressFormat();
+
+  return `${bitcoin}|${lightning}|${ethereum}|${defichain}`;
+}
+
 export const Environment = {
   debug: true,
   defaultLanguage: "DE",
-  addressFormat:
-    /^((7|8)\w{33}|(t|d)\w{33}|(t|d)\w{41}|0x\w{40}|(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|(LNURL|LNDHUB)[A-Z0-9]{25,250}|LNNID[A-Z0-9]{66})$/,
+  addressFormat: new RegExp(`^(${getAllAddressFormat()})$`),
   signatureFormat: /^(.{87}=|[a-f0-9]{130}|[a-f0-9x]{132}|[a-z0-9]{104}|[a-z0-9]{140,146})$/,
   api: {
     baseUrl: "https://dev.api.dfx.swiss/v1",
